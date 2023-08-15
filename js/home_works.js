05c57007a8dae3a2795e1e56e1aa448b5df08008
ArslanFront-end <jusupovarslan04@gmail.com>
2023-08-15 20:10:40 +0600

    dz2

--- js/home_works.js
+++ js/home_works.js
@@ -24,21 +24,21 @@ let positionX = 0, positionY = 0
 
 
 const moveBlock = () => {
-    if (positionX <= 449 && positionY === 0){
+    if (positionX <= 440 && positionY === 0){
         positionX++
-        childBlock.style.marginLeft = positionX + 'px'
+        childBlock.style.left = `${positionX}px`
         setTimeout(moveBlock, 0)
-    } else if(positionY <= 449 && positionX > 449){
+    } else if(positionY <= 440 && positionX > 440){
         positionY++
-        childBlock.style.marginTop = positionY + 'px'
+        childBlock.style.top = `${positionY}px`
         setTimeout(moveBlock, 0)
     }else if (positionX >= 0){
         positionX--
-        childBlock.style.marginLeft = positionX + 'px'
+        childBlock.style.left = `${positionX}px`
         setTimeout(moveBlock, 0)
     }else if (positionY >= 0){
         positionY--
-        childBlock.style.marginTop = positionY + 'px'
+        childBlock.style.top = `${positionY}px`
         setTimeout(moveBlock, 0)
     }
 }
@@ -46,6 +46,66 @@ const moveBlock = () => {
 moveBlock()
 
 
+let minutes = document.getElementById('minutes')
+let seconds = document.getElementById('seconds')
+let mlSeconds = document.getElementById('ml-seconds')
+const startBtn = document.getElementById('start')
+const stopBtn = document.getElementById('stop')
+const resetBtn = document.getElementById('reset')
+const resume = document.getElementById('resume')
+
+minuteS = 0
+secondS = 0
+mlSecondS = 0
+
+startBtn.onclick = function () {
+    const timerId = setInterval(function () {
+            if (mlSecondS <= 59){
+                mlSecondS++
+                mlSeconds.innerHTML = mlSecondS.toString().padStart(2, "0"); 
+            } else if ( secondS <= 59){
+                mlSecondS = 0
+                secondS++
+                seconds.innerHTML = secondS.toString().padStart(2, "0") ;
+            } else if (minuteS <= 59) { 
+                secondS = 0
+                minuteS++
+                minutes.innerHTML = minuteS.toString().padStart(2, "0");
+            }
+    }, 10)
+    stopBtn.onclick = function () {
+        clearInterval(timerId);
+    }
+    resetBtn.onclick = function () {
+        clearInterval(timerId)
+        minuteS = 0
+        secondS = 0
+        mlSecondS = 0
+        minutes.innerHTML = "00"
+        seconds.innerHTML = "00"
+        mlSeconds.innerHTML = "00"
+    }
+    resume.onclick = function () {
+        const timerId = setInterval(function () {
+            if (mlSecondS <= 59){
+                mlSecondS++
+                mlSeconds.innerHTML = mlSecondS.toString().padStart(2, "0"); 
+            } else if ( secondS <= 59){
+                mlSecondS = 0
+                secondS++
+                seconds.innerHTML = secondS.toString().padStart(2, "0") ;
+            } else if (minuteS <= 59) { 
+                secondS = 0
+                minuteS++
+                minutes.innerHTML = minuteS.toString().padStart(2, "0");
+            }
+    }, 10)
+    stopBtn.onclick = function () {
+        clearInterval(timerId);
+    }
+    }
+} 
+//test
 
 
 
@@ -66,28 +126,3 @@ moveBlock()
 
 
 
-
-
-// const moveBlock = () => {
-//     if (positionX <= 450 && positionY === 0){
-//         positionX++
-//         // console.log(positionX)
-//         childBlock.style.left = positionX + 'px'
-//         setTimeout(moveBlock, -900)
-    
-//     } else if (positionY <= 450) {
-//         positionY++
-//         console.log(positionY);
-//         childBlock.style.top = positionY + 'px'
-//         setTimeout(moveBlock, 0)
-//     } else if (positionX >= 0 ){
-//         positionX--
-//         childBlock.style.left = positionX + 'px'
-//         setTimeout(moveBlock, 0)
-//     } else if (positionY > 0) {
-//         positionY--
-//         childBlock.style.top = positionY + 'px'
-//         setTimeout(moveBlock,)
-//     }
-// }
-// moveBlock()
